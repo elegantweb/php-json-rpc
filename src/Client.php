9e192cf8e97443bee82b1adad643f7ea547530ed
Sina Sharifzade <sharifzadesina@gmail.com>
2020-02-13 12:03:38 +0330

    Update Namespace

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Elegant\Rpc;
+namespace Elegant\JsonRpc;
 
 use Exception;
 
--- src/HttpClient.php
+++ src/HttpClient.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Elegant\Rpc;
+namespace Elegant\JsonRpc;
 
 use GuzzleHttp\Client as HttpClient;
 
